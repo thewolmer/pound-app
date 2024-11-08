342d428429c2810909a017effdeccfa046716935
Armands Balodis <armands.balodis@gmail.com>
2024-11-08 12:43:39 +0200

    add db types

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,11 +2,12 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import { Env } from '~/config/env';
+import type { Database } from '~/types/database.types';
 
 const supabaseUrl = Env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = Env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
 	auth: {
 		storage: AsyncStorage,
 		autoRefreshToken: true,
---
+++ types/database.types.ts
@@ -0,0 +1,360 @@
+export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
+
+export type Database = {
+	public: {
+		Tables: {
+			account: {
+				Row: {
+					balance: number;
+					closed_at: string | null;
+					created_at: string;
+					id: string;
+					person_id: string | null;
+					status: string;
+					type: Database['public']['Enums']['account_type'];
+				};
+				Insert: {
+					balance?: number;
+					closed_at?: string | null;
+					created_at?: string;
+					id?: string;
+					person_id?: string | null;
+					status?: string;
+					type: Database['public']['Enums']['account_type'];
+				};
+				Update: {
+					balance?: number;
+					closed_at?: string | null;
+					created_at?: string;
+					id?: string;
+					person_id?: string | null;
+					status?: string;
+					type?: Database['public']['Enums']['account_type'];
+				};
+				Relationships: [
+					{
+						foreignKeyName: 'account_person_id_fkey';
+						columns: ['person_id'];
+						isOneToOne: false;
+						referencedRelation: 'person';
+						referencedColumns: ['id'];
+					},
+				];
+			};
+			expo_push_token: {
+				Row: {
+					created_at: string;
+					expo_push_token: string;
+					id: string;
+					person_id: string | null;
+				};
+				Insert: {
+					created_at?: string;
+					expo_push_token: string;
+					id?: string;
+					person_id?: string | null;
+				};
+				Update: {
+					created_at?: string;
+					expo_push_token?: string;
+					id?: string;
+					person_id?: string | null;
+				};
+				Relationships: [
+					{
+						foreignKeyName: 'expo_token_person_id_fkey';
+						columns: ['person_id'];
+						isOneToOne: false;
+						referencedRelation: 'person';
+						referencedColumns: ['id'];
+					},
+				];
+			};
+			notification: {
+				Row: {
+					body: string | null;
+					created_at: string;
+					data: Json | null;
+					id: string;
+					person_id: string | null;
+					title: string | null;
+					type: string | null;
+				};
+				Insert: {
+					body?: string | null;
+					created_at?: string;
+					data?: Json | null;
+					id?: string;
+					person_id?: string | null;
+					title?: string | null;
+					type?: string | null;
+				};
+				Update: {
+					body?: string | null;
+					created_at?: string;
+					data?: Json | null;
+					id?: string;
+					person_id?: string | null;
+					title?: string | null;
+					type?: string | null;
+				};
+				Relationships: [
+					{
+						foreignKeyName: 'notification_person_id_fkey';
+						columns: ['person_id'];
+						isOneToOne: false;
+						referencedRelation: 'person';
+						referencedColumns: ['id'];
+					},
+				];
+			};
+			person: {
+				Row: {
+					avatar_url: string | null;
+					created_at: string;
+					date_of_birth: string | null;
+					email: string;
+					first_name: string | null;
+					id: string;
+					identity_tag: string | null;
+					last_name: string | null;
+					middle_name: string | null;
+					phone: string | null;
+					status: string | null;
+					updated_at: string | null;
+				};
+				Insert: {
+					avatar_url?: string | null;
+					created_at?: string;
+					date_of_birth?: string | null;
+					email: string;
+					first_name?: string | null;
+					id?: string;
+					identity_tag?: string | null;
+					last_name?: string | null;
+					middle_name?: string | null;
+					phone?: string | null;
+					status?: string | null;
+					updated_at?: string | null;
+				};
+				Update: {
+					avatar_url?: string | null;
+					created_at?: string;
+					date_of_birth?: string | null;
+					email?: string;
+					first_name?: string | null;
+					id?: string;
+					identity_tag?: string | null;
+					last_name?: string | null;
+					middle_name?: string | null;
+					phone?: string | null;
+					status?: string | null;
+					updated_at?: string | null;
+				};
+				Relationships: [];
+			};
+			transaction: {
+				Row: {
+					amount: number;
+					created_at: string;
+					destination_account_id: string | null;
+					destination_new_balance: number;
+					destination_old_balance: number;
+					id: string;
+					is_flagged_fraud: boolean;
+					is_fraud: boolean;
+					origin_account_id: string | null;
+					origin_new_balance: number;
+					origin_old_balance: number;
+					reference: string;
+					status: Database['public']['Enums']['transaction_status'];
+					type: Database['public']['Enums']['transaction_type'];
+				};
+				Insert: {
+					amount: number;
+					created_at?: string;
+					destination_account_id?: string | null;
+					destination_new_balance?: number;
+					destination_old_balance?: number;
+					id?: string;
+					is_flagged_fraud?: boolean;
+					is_fraud?: boolean;
+					origin_account_id?: string | null;
+					origin_new_balance?: number;
+					origin_old_balance?: number;
+					reference: string;
+					status: Database['public']['Enums']['transaction_status'];
+					type: Database['public']['Enums']['transaction_type'];
+				};
+				Update: {
+					amount?: number;
+					created_at?: string;
+					destination_account_id?: string | null;
+					destination_new_balance?: number;
+					destination_old_balance?: number;
+					id?: string;
+					is_flagged_fraud?: boolean;
+					is_fraud?: boolean;
+					origin_account_id?: string | null;
+					origin_new_balance?: number;
+					origin_old_balance?: number;
+					reference?: string;
+					status?: Database['public']['Enums']['transaction_status'];
+					type?: Database['public']['Enums']['transaction_type'];
+				};
+				Relationships: [
+					{
+						foreignKeyName: 'transaction_destination_account_id_fkey';
+						columns: ['destination_account_id'];
+						isOneToOne: false;
+						referencedRelation: 'account';
+						referencedColumns: ['id'];
+					},
+					{
+						foreignKeyName: 'transaction_origin_account_id_fkey';
+						columns: ['origin_account_id'];
+						isOneToOne: false;
+						referencedRelation: 'account';
+						referencedColumns: ['id'];
+					},
+				];
+			};
+		};
+		Views: {
+			[_ in never]: never;
+		};
+		Functions: {
+			make_deposit: {
+				Args: {
+					amount: number;
+					destination_account_id: string;
+					reference: string;
+				};
+				Returns: undefined;
+			};
+			make_transfer: {
+				Args: {
+					amount: number;
+					origin_account_id: string;
+					destination_account_id: string;
+					reference: string;
+				};
+				Returns: undefined;
+			};
+		};
+		Enums: {
+			account_type: 'individual';
+			transaction_status:
+				| 'pending'
+				| 'in_progress'
+				| 'completed'
+				| 'failed'
+				| 'canceled'
+				| 'on_hold'
+				| 'reversed'
+				| 'expired'
+				| 'refunded';
+			transaction_type:
+				| 'deposit'
+				| 'withdrawal'
+				| 'transfer'
+				| 'payment'
+				| 'purchase'
+				| 'refund'
+				| 'fee'
+				| 'interest'
+				| 'cashback'
+				| 'adjustment'
+				| 'chargeback'
+				| 'reversal';
+		};
+		CompositeTypes: {
+			[_ in never]: never;
+		};
+	};
+};
+
+type PublicSchema = Database[Extract<keyof Database, 'public'>];
+
+export type Tables<
+	PublicTableNameOrOptions extends keyof (PublicSchema['Tables'] & PublicSchema['Views']) | { schema: keyof Database },
+	TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+		? keyof (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+				Database[PublicTableNameOrOptions['schema']]['Views'])
+		: never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+	? (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+			Database[PublicTableNameOrOptions['schema']]['Views'])[TableName] extends {
+			Row: infer R;
+		}
+		? R
+		: never
+	: PublicTableNameOrOptions extends keyof (PublicSchema['Tables'] & PublicSchema['Views'])
+		? (PublicSchema['Tables'] & PublicSchema['Views'])[PublicTableNameOrOptions] extends {
+				Row: infer R;
+			}
+			? R
+			: never
+		: never;
+
+export type TablesInsert<
+	PublicTableNameOrOptions extends keyof PublicSchema['Tables'] | { schema: keyof Database },
+	TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+		? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+		: never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+	? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+			Insert: infer I;
+		}
+		? I
+		: never
+	: PublicTableNameOrOptions extends keyof PublicSchema['Tables']
+		? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
+				Insert: infer I;
+			}
+			? I
+			: never
+		: never;
+
+export type TablesUpdate<
+	PublicTableNameOrOptions extends keyof PublicSchema['Tables'] | { schema: keyof Database },
+	TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+		? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+		: never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+	? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+			Update: infer U;
+		}
+		? U
+		: never
+	: PublicTableNameOrOptions extends keyof PublicSchema['Tables']
+		? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
+				Update: infer U;
+			}
+			? U
+			: never
+		: never;
+
+export type Enums<
+	PublicEnumNameOrOptions extends keyof PublicSchema['Enums'] | { schema: keyof Database },
+	EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+		? keyof Database[PublicEnumNameOrOptions['schema']]['Enums']
+		: never = never,
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+	? Database[PublicEnumNameOrOptions['schema']]['Enums'][EnumName]
+	: PublicEnumNameOrOptions extends keyof PublicSchema['Enums']
+		? PublicSchema['Enums'][PublicEnumNameOrOptions]
+		: never;
+
+export type CompositeTypes<
+	PublicCompositeTypeNameOrOptions extends keyof PublicSchema['CompositeTypes'] | { schema: keyof Database },
+	CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+		schema: keyof Database;
+	}
+		? keyof Database[PublicCompositeTypeNameOrOptions['schema']]['CompositeTypes']
+		: never = never,
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+	? Database[PublicCompositeTypeNameOrOptions['schema']]['CompositeTypes'][CompositeTypeName]
+	: PublicCompositeTypeNameOrOptions extends keyof PublicSchema['CompositeTypes']
+		? PublicSchema['CompositeTypes'][PublicCompositeTypeNameOrOptions]
+		: never;

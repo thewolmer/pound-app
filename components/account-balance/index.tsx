622af08435dd9817ab285ccc883de02a87b91e68
Armands Balodis <armands.balodis@gmail.com>
2024-11-11 14:12:38 +0200

    feat: add green/red to account balance change

--- components/account-balance/index.tsx
+++ components/account-balance/index.tsx
@@ -1,21 +1,50 @@
+import { useEffect, useState } from 'react';
 import { Text } from '~/components/ui/text';
 import { H1 } from '~/components/ui/typography';
 import { useAccount } from '~/context/AccountContext';
 import { formatCurrency } from '~/lib/formatCurrency';
+import { usePrevious } from '~/lib/usePrevious';
 import { Card, CardFooter, CardHeader } from '../ui/card';
 import DepositButton from './DepositButton';
 import RequestButton from './RequestButton';
 import SendButton from './SendButton';
 
 export function AccountBalance() {
-	const { balance } = useAccount();
+	const { balance, isLoading } = useAccount();
+	const [previousBalance, setPreviousBalance] = useState<number | null>(null);
+	const [isChanged, setIsChanged] = useState(false);
+
+	useEffect(() => {
+		if (isLoading) return;
+		if (previousBalance === null) {
+			setPreviousBalance(balance);
+			return;
+		}
+
+		if (balance !== previousBalance) {
+			setIsChanged(true);
+
+			const timer = setTimeout(() => {
+				setPreviousBalance(balance);
+				setIsChanged(false);
+			}, 1000);
+
+			return () => clearTimeout(timer);
+		}
+	}, [balance, previousBalance, isLoading]);
+
+	const getBalanceColor = () => {
+		if (!isChanged || balance === previousBalance) return '';
+
+		return balance > (previousBalance || 0) ? 'text-success-foreground' : 'text-destructive-foreground';
+	};
 
 	return (
 		<>
 			<Card>
 				<CardHeader className="items-center">
 					<Text className="mb-2 text-accent-foreground">Available Balance</Text>
-					<H1>{formatCurrency(Number(balance))}</H1>
+					<H1 className={getBalanceColor()}>{formatCurrency(Number(balance))}</H1>
 				</CardHeader>
 
 				<CardFooter className="flex justify-between">

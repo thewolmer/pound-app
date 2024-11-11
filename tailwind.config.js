a0833b6ee1e16100309d405c4a462fc25e8193a8
Wol <real.krn@outlook.com>
2024-11-11 13:20:23 +0530

    style: theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,14 @@ module.exports = {
 					DEFAULT: 'hsl(var(--destructive))',
 					foreground: 'hsl(var(--destructive-foreground))',
 				},
+				success: {
+					DEFAULT: 'hsl(var(--success))',
+					foreground: 'hsl(var(--success-foreground))',
+				},
+				info: {
+					DEFAULT: 'hsl(var(--info))',
+					foreground: 'hsl(var(--info-foreground))',
+				},
 				muted: {
 					DEFAULT: 'hsl(var(--muted))',
 					foreground: 'hsl(var(--muted-foreground))',

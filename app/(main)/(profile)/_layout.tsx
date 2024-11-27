8b7560b5838f8777ca18cc66d0a84b74d7491285
Wol <contact@wolmer.me>
2024-11-27 15:24:09 +0530

    fix: add 20px hitSlop for back buttons

--- app/(main)/(profile)/_layout.tsx
+++ app/(main)/(profile)/_layout.tsx
@@ -11,7 +11,7 @@ export default function ProfileLayout() {
 					title: 'Profile',
 					headerLargeTitle: true,
 					headerRight: ({ tintColor }) => (
-						<Pressable onPress={() => router.push('/(profile)/settings')}>
+						<Pressable hitSlop={20} onPress={() => router.push('/(profile)/settings')}>
 							<TabBarIcon name="settings-outline" color={tintColor} />
 						</Pressable>
 					),
